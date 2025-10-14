296a37673ab79859ac3b38a6f8eb8fad553932cd
JN-64 <juanprogamern64@gmail.com>
2025-10-14 18:12:15 -0500

    Add files via upload

---
+++ Ejercicio_1.java
@@ -0,0 +1,25 @@
+
+package Secuenciales;
+
+/**
+ *
+ * @author Juan José Giraldo M.
+ */
+public class Ejercicio_1 {
+
+    public static void main(String[] args) {
+
+        //Crear variables
+        String username="Celeste";
+        int cofreUno=8;
+        int cofreDos=12;
+        int sumaDulces;
+        
+        //Proceso Tilín777
+        sumaDulces=cofreUno + cofreDos;
+        
+        //Salida
+        System.out.print(username + " ahora tiene un total de "+ sumaDulces +" caramelos magicos");
+    }
+    
+}
---
+++ Ejercicio_2.java
@@ -0,0 +1,25 @@
+
+package Secuenciales;
+
+/**
+ *
+ * @author Juan José
+ */
+public class Ejercicio_2 {
+
+    public static void main(String[] args) {
+    
+        //Crear variables
+        String username="Aprendiz Harry";
+        double retoUno=3.5;
+        double retoDos=4.2;
+        double retoTres=2.8;
+        double sumaNotas;
+        //Proceso Tilín777
+        sumaNotas=(retoUno + retoDos + retoTres) / 3;
+        //Salida
+        System.out.print(username + " tu promedio final es "+ sumaNotas + " ¡Nivel superado felicitaciones!");
+    }
+    
+}
+
