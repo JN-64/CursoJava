f1e6a2a7414b7194850c2a8eb7f8bc523bddb8e2
JN-64 <juanprogamern64@gmail.com>
2025-10-15 17:26:53 -0500

    Add files via upload

---
+++ Ejercicio_3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Secuenciales;
+
+/**
+ *
+ * @author Juan José Giraldo
+ */
+public class Ejercicio_3 {
+
+    public static void main(String[] args) {
+        
+        int salario=2000000;
+        double descuentoSalud=salario*0.04;
+        double descuentodePension=salario*0.05;
+        double SaldoFinal;
+        SaldoFinal=descuentoSalud + descuentodePension;
+        System.out.print("Tu saldo final es de " + SaldoFinal + " ¡Preparate para la siguiente aventura!");
+    }
+    
+}
